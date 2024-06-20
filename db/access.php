1e6f813ae6c0228ce56bd75036e01e0298b4e333
brianlmerritt <brianlmerritt@noreply.com>
2024-06-20 12:28:05 +0100

    Adding capabilities to block_rvc_survey

---
+++ db/access.php
@@ -0,0 +1,23 @@
+<?php
+
+$capabilities = array(
+
+    'block/rvc_survey:myaddinstance' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'user' => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' => 'moodle/my:manageblocks'
+    ),
+
+    'block/rvc_survey:addinstance' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ),
+);
